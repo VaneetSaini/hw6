3aa3c8ea353e56812f2b4eaef0c274b192ada4ef
Vaneet Saini <vsaini@usc.edu>
2022-12-08 21:48:45 +0000

    submit boggle

--- boggle.cpp
+++ boggle.cpp
@@ -92,6 +92,40 @@ std::set<std::string> boggle(const std::set<std::string>& dict, const std::set<s
 bool boggleHelper(const std::set<std::string>& dict, const std::set<std::string>& prefix, const std::vector<std::vector<char> >& board, 
 								   std::string word, std::set<std::string>& result, unsigned int r, unsigned int c, int dr, int dc)
 {
-//add your solution here!
+	unsigned int size = board.size();
+	if (size == 0) return false;
 
+
+	word += board[r][c];
+
+	if (r+dr >= size || c+dc >= size)
+	{
+		if (dict.find(word) != dict.end())
+		{
+			result.insert(word);
+			return true;
+		}
+		return false;
+	}
+
+	if (dict.find(word) != dict.end() && prefix.find(word) == prefix.end())
+	{
+		result.insert(word);
+		return true;
+	}
+
+	else if (dict.find(word) != dict.end())
+	{
+		if (boggleHelper(dict, prefix, board, word, result, r+dr, c+dc, dr, dc) == false) result.insert(word);
+		return true;
+	}
+
+	else if (prefix.find(word) != prefix.end())
+	{
+		if (boggleHelper(dict, prefix, board, word, result, r+dr, c+dc, dr, dc) == true) return false;
+		return false;
+	}
+	
+	word = word.substr(0, word.length() - 1);
+	return false;
 }
